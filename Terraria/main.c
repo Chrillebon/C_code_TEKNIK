c16b9f900f45876e48151ded82a2122a038f28fe
Chrillebon <christian.mikkelstrup@gmail.com>
2017-11-10 16:37:45 +0100

    test af filer og animation m. bogstaver

--- Terraria/main.c
+++ Terraria/main.c
@@ -6,21 +6,13 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
-#include <string>
 #include "displays.h"
 
 void initialize(){
-	DDRC = 0xFF; //alle GPIO på port D sat til outputs, dette er vores output tal
-	DDRD = 0b00010000; // PC5+Pc4 sat til input
-	DDRB = 0x00;
+	DDRD = 0xFF; //alle GPIO på port D sat til outputs, dette er vores output tal
+	DDRC = 0b00010000; // PC5+Pc4 sat til input
+	//DDRB = 0x00;
 }
-
-volatile int choices[4] = {zero,two,three,four};
-
-/*
-eksempel på hvordan dette ændres:
-choices[0] = A;
-*/
 
 /*
 genstande der skal integreres:
@@ -35,9 +27,17 @@ Krystal (B6, B7)
 int main(void)
 {
 	initialize();
+	volatile int choices[4] = {NUMBERS[0],NUMBERS[0],NUMBERS[0],NUMBERS[0]};
+	/*
+	eksempel på hvordan dette ændres:
+	choices[0] = A;
+	*/
+
+	volatile int k=0;
+	volatile int n=1;
 
 	while(1)
-	{ //for eeeeeevigt!!
+	{
 		//-------------------------------------------
 		//humidity sensor:
 
@@ -52,15 +52,44 @@ int main(void)
 
 		//-------------------------------------------
 		//displays der skifter og viser tal/bogstaver:
-		for(int i=0;i<4;i++)
+		for(int i=0;i<2;i++)
+		{
+			DDRC = DISPLAYS[i];
+			PORTD = choices[i];
+			k++;
+			_delay_ms(1);
+		}
+		if(k>1000)
+		{
+			if(n<=9)
+			{
+				choices[0]=choices[1];
+				choices[1]=NUMBERS[n];
+				n++;
+			}
+			else if(n>9)
+			{
+				choices[0]=choices[1];
+				choices[1]=ALPHABET[n-10];
+				n++;
+			}
+			if(n>35)
+			{
+				n=0;
+			}
+			k=0;
+		}
+		/*
+		for(int i=0;i<26;i++)
 		{
 			DDRC = displays[i];
 			PORTD = NUMBERS[choices[i]];
 			_delay_ms(1);
 		}
+		*/
 		//--------------------------------------------
 		//Knapper og interface:
-		
+
 		//--------------------------------------------
 	}
 }
