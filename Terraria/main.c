6d367f3d80d8eab66cb130149c4965bcbd546c82
Christian Møller Mikkelstrup <chri94f2@edu.nextkbh.dk>
2017-11-17 09:34:28 +0100

    Display færdig indtil videre

--- Terraria/main.c
+++ Terraria/main.c
@@ -26,6 +26,35 @@ Knappper (8 stk. A0-7) - lys, fugt, luft, status, reset, op, indst., ned
 
 */
 
+//Sørger for, at ingen værdier kan være uden for "range"
+void range()
+{
+	if(Size_Air < 0)
+	{
+		Size_Air = 0;
+	}
+	if(Size_Air > 10)
+	{
+		Size_Air = 10;
+	}
+	if(Size_Humid < 0)
+	{
+		Size_Humid = 0;
+	}
+	if(Size_Humid > 100)
+	{
+		Size_Humid = 100;
+	}
+	if(Size_Light < 0)
+	{
+		Size_Light = 0;
+	}
+	if(Size_Light > 24)
+	{
+		Size_Light = 24;
+	}
+}
+
 // renser input, så vi kan bruge det
 void rensinput()
 {
@@ -74,13 +103,16 @@ int main(void)
 	volatile int Status_Humid = 0;
 	volatile int Status_Air = 0;
 	volatile int Status_Setting = 0;
+	volatile int Status_Reset = 0;
 
 	volatile int Size_Status = 0;
 	//Dage siden start
 	volatile int Size_Light = 16;
 	//Timer 0-24
 	volatile int Size_Humid = 50;
-	//Procent 0-100
+	//Procent 0-100 - hvad vi vil have at fugtigheden er
+	volatile int Size_Humid_Sensor = 0;
+	//Procent 0-100 - hvad vi måler at luftfugtigheden skal være
 	volatile int Size_Air = 0;
 	//Størrelse fra 0-10
 
@@ -107,32 +139,12 @@ int main(void)
 			display(t);
 			DDRC = DISPLAYS[i];
 			PORTD = choices[i];
+			//Timere der tæller opad
 			Time++;
 			Global_Time++;
 			Time_Days++;
 			_delay_ms(1);
 		}
-		//test af bibliotek-----------------------------------
-		/*if(Global_Time>1000)
-		{
-			if(n<=9)
-			{
-				choices[0]=choices[1];
-				choices[1]=NUMBERS[n];
-				n++;
-			}
-			else if(n>9)
-			{
-				choices[0]=choices[1];
-				choices[1]=ALPHABET[n-10];
-				n++;
-			}
-			if(n>35)
-			{
-				n=0;
-			}
-			Global_Time=0;
-		}*/
 		//--------------------------------------------
 		//Knapper og interface:
 
@@ -165,10 +177,15 @@ int main(void)
 		if(PINA == 0b00001000)
 		{
 			//Status
+			//Denne aktiverer kun, hvis det er tid til at høste
 			if(Status_Status == 2)
 			{
 				Size_Status = 0;
+				memset(SoonToBe,0x00,strlen(SoonToBe));
+				SoonToBe = "HOEST FAERDIG - HOEST IGEN OM 8 UGER";
+				rensinput();
 			}
+			//alle andre gange:
 			else
 			{
 				Status_Status = 1;
@@ -178,87 +195,175 @@ int main(void)
 			}
 			Time = 0;
 		}
+
+		//Det næste er ikke en knap, men hører med til "reset" knappen.
+		//Sørger for, at der går tid fra man trykker på reset første gang til at man kan hard resette.
+		if(Status_Status == 1 && Status_Reset == 1)
+		{
+			//forsinker i 1/3 af et sekund, så man forhåbentligt ved hvad man laver/har fået en advarsel
+			_delay_ms(333);
+		}
 		if(PINA == 0b00010000)
 		{
 			//Reset
+			if(Status_Status == 1 && Status_Reset == 0)
+			{
+				Status_Reset = 1;
+				memset(SoonToBe,0x00,strlen(SoonToBe));
+				SoonToBe = "TRYK IGEN FOR HARD RESET";
+				rensinput();
+			}
+			//Nu har man trykket to gange på reset under indstillinger og den er blevet hard resat:
+			if(Status_Status == 1 && Status_Reset == 1)
+			{
+				Size_Status = 0;
+				memset(SoonToBe,0x00,strlen(SoonToBe));
+				SoonToBe = "HARD RESET FAERDIG";
+				rensinput();
+			}
+			else
+			{
+				memset(SoonToBe,0x00,strlen(SoonToBe));
+				SoonToBe = "RESET FAERDIG";
+				rensinput();
+			}
+			//Hvis det er tid til at høste
 			if(Status_Status == 2)
 			{
 				Time_Days = 0;
+				Size_Status = 0;
+				//Sørger for, at status ikke aktiverer længere nede:
+				Status_Status = 0;
+				memset(SoonToBe,0x00,strlen(SoonToBe));
+				SoonToBe = "HOEST FAERDIG - HOEST IGEN OM 8 UGER";
+				rensinput();
 			}
-			Status_Status = 0;
-			Status_Air = 0;
-			Status_Humid = 0;
-			Status_Light = 0;
-			Global_Time=0;
-			Global_Time_Keeper = 0;
-			t = 0;
+			//i alle andre tilfælde:
+			else
+			{
+				Status_Status = 0;
+				Status_Air = 0;
+				Status_Humid = 0;
+				Status_Light = 0;
+				Global_Time=0;
+				Global_Time_Keeper = 0;
+				t = 0;
+				//sætter alle størrelser til standard:
+				Size_Status = 0;
+				//Uger siden start
+				Size_Light = 16;
+				//Timer 0-24
+				Size_Humid = 50;
+				//Procent 0-100
+				Size_Air = 0;
+				//Størrelse fra 0-10
+			}
 			Time = 0;
-			//sætter alle størrelser til standard:
-			Size_Status = 0;
-			//Uger siden start
-			Size_Light = 16;
-			//Timer 0-24
-			Size_Humid = 50;
-			//Procent 0-100
-			Size_Air = 0;
-			//Størrelse fra 0-10
 		}
 		if(PINA == 0b00100000)
 		{
 			//Op
 			if(Status_Air == 2)
 			{
+				//Justerer størrelsen af variablen
 				Size_Air += 1;
+				//Sørger for, at den ikke kan være uden for "range"
+				range();
+				//Skriver det på displayet
+				memset(SoonToBe,0x00,strlen(SoonToBe));
+				SoonToBe = Size_Air << " - LUFT UD AF "<<NUMBERS[1]<<NUMBERS[0];
+				rensinput();
 			}
 			if(Status_Humid == 2)
 			{
+				//Justerer størrelsen af variablen
 				Size_Humid += 5;
+				//Sørger for, at den ikke kan være uden for "range"
+				range();
+				//Skriver det på displayet
+				memset(SoonToBe,0x00,strlen(SoonToBe));
+				SoonToBe = Size_Humid << " PROCENT FUGT";
+				rensinput();
 			}
 			if(Status_Light == 2)
 			{
+				//Justerer variablen
 				Size_Light += 1;
+				//Sørger for, at den ikke kan være uden for "range"
+				range();
+				//Skriver det på skærmen
+				memset(SoonToBe,0x00,strlen(SoonToBe));
+				SoonToBe = Size_Light<< " TIMER LYS OM DAGEN";
+				rensinput();
 			}
 			Time = 0;
 		}
 		if(PINA == 0b01000000)
 		{
 			//Indstillinger
+			//registrerer at man trykker
 			Status_Setting = 1;
 			Status_Status = 0;
 			Status_Air = 0;
 			Status_Humid = 0;
 			Status_Light = 0;
 			Time = 0;
+			memset(SoonToBe,0x00,strlen(SoonToBe));
+			SoonToBe = "HVAD VIL DU JUSTERE";
+			rensinput();
 		}
 		if(PINA == 0b10000000)
 		{
 			//Ned
 			if(Status_Air == 2)
 			{
+				//Justerer størrelsen af variablen
 				Size_Air -= 1;
+				//Sørger for, at den ikke kan være uden for "range"
+				range();
+				//Skriver det på displayet
+				memset(SoonToBe,0x00,strlen(SoonToBe));
+				SoonToBe = Size_Air << " - LUFT UD AF "<<NUMBERS[1]<<NUMBERS[0];
+				rensinput();
 			}
 			if(Status_Humid == 2)
 			{
+				//Justerer størrelsen af variablen
 				Size_Humid -= 5;
+				//Sørger for, at den ikke kan være uden for "range"
+				range();
+				//Skriver det på displayet
+				memset(SoonToBe,0x00,strlen(SoonToBe));
+				SoonToBe = Size_Humid << " PROCENT FUGT";
+				rensinput();
 			}
 			if(Status_Light == 2)
 			{
+				//Justerer variablen
 				Size_Light -= 1;
+				//Sørger for, at den ikke kan være uden for "range"
+				range();
+				//Skriver det på skærmen
+				memset(SoonToBe,0x00,strlen(SoonToBe));
+				SoonToBe = Size_Light<< " TIMER LYS OM DAGEN";
+				rensinput();
 			}
 			Time = 0;
 		}
+		//Den virkende del af Status_Status:
 		if(Status_Status)
 		{
 			//Display status
 			memset(SoonToBe,0x00,strlen(SoonToBe));
 			SoonToBe = "HOEST OM " << 8*7-Size_Status << " DAGE";
+			rensinput();
 			Time = 0;
 		}
 		//undersøger om man forsøger at ændre en værdi
 		if(Status_Setting)
 		{
 			//finder hvilken værdi man vil ændre
-			//Ændrer herefter deres værdier
+			//Tillader herefter at man kan ændre dem med "op" og "ned"
 			if(Status_Air)
 			{
 				Status_Air = 2;
@@ -281,6 +386,7 @@ int main(void)
 		}
 		else
 		{
+			//Hvad displayet skal vise efter man har trykket på knapperne:
 			if(Status_Air)
 			{
 				memset(SoonToBe,0x00,strlen(SoonToBe));
@@ -300,32 +406,6 @@ int main(void)
 		}
 
 		//Slut på hvad knapperne gør
-
-		//Sørger for, at ingen værdier kan være uden for "range"
-		if(Size_Air < 0)
-		{
-			Size_Air = 0;
-		}
-		if(Size_Air > 10)
-		{
-			Size_Air = 10;
-		}
-		if(Size_Humid < 0)
-		{
-			Size_Humid = 0;
-		}
-		if(Size_Humid > 100)
-		{
-			Size_Humid = 100;
-		}
-		if(Size_Light < 0)
-		{
-			Size_Light = 0;
-		}
-		if(Size_Light > 24)
-		{
-			Size_Light = 24;
-		}
 
 		//Hvis det er tid til at høste:
 
@@ -367,8 +447,8 @@ int main(void)
 		}
 
 		//counter der holder styr på hvor langt bogstaverne i den tekst man skal skrive er kommet
-		//Alle rykker en plads hver 0.75 sek.
-		if(Global_Time_Keeper > 0.75*1000)
+		//Alle rykker en plads hver 0.5 sek.
+		if(Global_Time_Keeper > 0.5*1000)
 		{
 			t++;
 			Global_Time_Keeper = 0;
